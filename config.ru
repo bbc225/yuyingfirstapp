c2a59e65dd4456cf9f7024c89c2c50093a386d0d
Gladys <924267325@qq.com>
2017-11-21 11:17:05 +0800

    two

---
+++ config.ru
@@ -0,0 +1,3 @@
+require './mytest.rb'
+
+run Mytestapp
--- mytest.rb
+++ mytest.rb
@@ -1,92 +1,95 @@
 require 'sinatra'
+require 'sinatra/base'
 require 'slim'
 require 'sass'
 require 'mysql2'
 require 'active_record'
 require_relative 'mydata.rb'
 
-configure do
-	enable :sessions
-	set :username, 'frank'
-	set :password, 'sinatra'
-end
+class Mytestapp < Sinatra::Base
+	configure do
+		enable :sessions
+		set :username, 'frank'
+		set :password, 'sinatra'
+	end
 
-get('/styles.css'){ scss :styles }
+	get('/styles.css'){ scss :styles }
 
-get '/login' do
-	slim :login
-end
-
-post '/login' do
-	if params[:username] == settings.username && params[:password] == settings.password
-		session[:admin] = true
-		redirect to('/songs')
-	else
+	get '/login' do
 		slim :login
 	end
-end
 
-get '/logout' do
-	session.clear
-	redirect to('/login')
-end
+	post '/login' do
+		if params[:username] == settings.username && params[:password] == settings.password
+			session[:admin] = true
+			redirect to('/songs')
+		else
+			slim :login
+		end
+	end
 
-get '/' do
-	slim :home
-end
+	get '/logout' do
+		session.clear
+		redirect to('/login')
+	end
 
-get '/about' do
-	@title = "All About This Website"
-	slim :about
-end
-get '/contact' do
-	slim :contact
-end
+	get '/' do
+		slim :home
+	end
 
-not_found do
-	slim :not_found
-end
+	get '/about' do
+		@title = "All About This Website"
+		slim :about
+	end
+	get '/contact' do
+		slim :contact
+	end
 
-
-get '/songs' do
-	@songs = Song.all
-	slim :songs
-end
-
-get '/songs/new' do
-	halt(401,'Not Authorized') unless session[:admin]
-	@song = Song.new
-	slim :new_song
-end
-
-get '/songs/:id' do
-	halt(401,'Not Authorized') unless session[:admin]
-	@song = Song.find_by(id: params[:id])
-	slim :show_song
-end
+	not_found do
+		slim :not_found
+	end
 
 
-post '/songs' do
-	halt(401,'Not Authorized') unless session[:admin]
-	song = Song.create(params[:song])
-	redirect to("/songs/#{song.id}")
-end
+	get '/songs' do
+		@songs = Song.all
+		slim :songs
+	end
 
-get '/songs/:id/edit' do
-	halt(401,'Not Authorized') unless session[:admin]
-	@song = Song.find_by(id: params[:id])
-	slim :edit_song
-end
+	get '/songs/new' do
+		halt(401,'Not Authorized') unless session[:admin]
+		@song = Song.new
+		slim :new_song
+	end
 
-put '/songs/:id' do
-	halt(401,'Not Authorized') unless session[:admin]
-	song = Song.find_by(id: params[:id])
-	song.update(params[:song])
-	redirect to("/songs/#{song.id}")
-end
+	get '/songs/:id' do
+		halt(401,'Not Authorized') unless session[:admin]
+		@song = Song.find_by(id: params[:id])
+		slim :show_song
+	end
 
-delete '/songs/:id' do
-	halt(401,'Not Authorized') unless session[:admin]
-	Song.find_by(id: params[:id]).destroy
-	redirect to('/songs')
+
+	post '/songs' do
+		halt(401,'Not Authorized') unless session[:admin]
+		song = Song.create(params[:song])
+		redirect to("/songs/#{song.id}")
+	end
+
+	get '/songs/:id/edit' do
+		halt(401,'Not Authorized') unless session[:admin]
+		@song = Song.find_by(id: params[:id])
+		slim :edit_song
+	end
+
+	put '/songs/:id' do
+		halt(401,'Not Authorized') unless session[:admin]
+		song = Song.find_by(id: params[:id])
+		song.update(params[:song])
+		redirect to("/songs/#{song.id}")
+	end
+
+	delete '/songs/:id' do
+		halt(401,'Not Authorized') unless session[:admin]
+		Song.find_by(id: params[:id]).destroy
+		redirect to('/songs')
+	end
 end
